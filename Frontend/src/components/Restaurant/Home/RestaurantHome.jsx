1e9d5c957b2b659ba271d1ae2b9323dfb2841bbf
vihitakesiraju <kesirajuvihita@gmail.com>
2020-10-10 17:24:47 -0700

    changes ui 2

--- Frontend/src/components/Restaurant/Home/RestaurantHome.jsx
+++ Frontend/src/components/Restaurant/Home/RestaurantHome.jsx
@@ -1,42 +1,54 @@
 import React, { Component } from "react";
 import "./RestaurantHome.styles.css";
+import menuIcon from "../../../Assets/BackgroundImages/menu_icon.jpg";
+import jumborest from "../../../Assets/BackgroundImages/rest_jumbo.jpg";
+
 class RestaurantHome extends Component {
   state = {};
   render() {
     return (
-      <div className="homeLayout">
-        <div className="homeGrid">
-          <a
-            href="/restaurant/menu"
-            class="sq list-group-item list-group-item-danger "
-          >
-            Menu
-          </a>
-          <a
-            href="/restaurant/reviews"
-            class="sq  list-group-item list-group-item-danger "
-          >
-            Reviews
-          </a>
-          <a
-            href="/restaurant/orders"
-            class="sq list-group-item list-group-item-danger "
-          >
-            Orders
-          </a>
-          <a
-            href="/restaurant/events/home"
-            class="sq list-group-item list-group-item-danger "
-          >
-            Events
-          </a>
-          <a
-            href="/restaurant/profile"
-            class="sq list-group-item list-group-item-danger "
-          >
-            Profile
-          </a>
-        </div>
+      <div class="image">
+        <nav class="navbar navbar-default navbar-fixed-top">
+          <div class="container">
+            <div class="navbar-header">
+              <a
+                class="navbar-brand"
+                style={{ color: "white" }}
+                href="/restaurant/menu/list"
+              >
+                Menu
+              </a>
+              <a
+                class="navbar-brand"
+                style={{ color: "white" }}
+                href="/restaurant/reviews"
+              >
+                Reviews
+              </a>
+              <a
+                class="navbar-brand"
+                style={{ color: "white" }}
+                href="/restaurant/orders"
+              >
+                Orders
+              </a>
+              <a
+                class="navbar-brand"
+                style={{ color: "white" }}
+                href="/restaurant/events/home"
+              >
+                Events
+              </a>
+              <a
+                class="navbar-brand"
+                style={{ color: "white" }}
+                href="/restaurant/profile"
+              >
+                Profile
+              </a>
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
