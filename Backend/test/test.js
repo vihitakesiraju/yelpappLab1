50aac07c27b296fe60821c29fa5dc7f46224f8ed
vihitakesiraju <kesirajuvihita@gmail.com>
2020-10-13 00:05:37 -0700

    final fix

--- Backend/test/test.js
+++ Backend/test/test.js
@@ -1,44 +1,93 @@
-var chai = require("chai"),
-  chaiHttp = require("chai-http");
+const { expect } = require("chai");
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+// let server = require('../server');
+let should = chai.should();
+let routeConstants = require("../config/routeConstants");
+
 chai.use(chaiHttp);
-const api_host = "http://localhost";
-const api_port = "3001";
-const api_url = api_host + ":" + api_port;
 
-const expect = require("chai").expect;
+describe("Restaurants Menu", () => {
+  describe("/restaurant/menu", () => {
+    it("GET all the restaurants", (done) => {
+      chai
+        .request(`${routeConstants.BACKEND_URL}`)
+        .get(`/restaurant${routeConstants.GET_ALL_RESTAURANTS}`)
+        .end((err, res) => {
+          expect(200);
+          done();
+        });
+    });
+  });
+});
 
-// it("Test server status", function (done) {
-//   chai
-//     .request(api_url)
-//     .post("/login")
-//     .send({ username: "Gustavo_Monk@exampl", password: "" })
+describe("Events Create", () => {
+  describe("/events/create", () => {
+    it("GET all the events", (done) => {
+      chai
+        .request(`${routeConstants.BACKEND_URL}`)
+        .get(`/events${routeConstants.GET_ALL_EVENTS}`)
+        .end((err, res) => {
+          expect(200);
+          done();
+        });
+    });
+  });
+});
 
-//     .end(function (err, res) {
-//       expect(200);
-//       done();
-//     });
-// });
+describe("login post", () => {
+  describe("login/", () => {
+    it("Give a an error on non existent user", (done) => {
+      let body = {
+        email_id: "The Lord of the Rings",
+        password: "J.R.R. Tolkien",
+      };
+      chai
+        .request(`${routeConstants.BACKEND_URL}`)
+        .post("/login")
+        .send(body)
+        .end((err, res) => {
+          expect(400);
 
-// it("get restaurant menu", function (done) {
-//   chai
-//     .request(api_url)
-//     .get("/restaurant/menu/list")
-//     .send()
-//     .end(function (res, err) {
-//       expect(200);
-//       done();
-//     });
-// });
+          done();
+        });
+    });
+  });
+});
 
-// it("get restaurant profile", function (done) {
-//   chai
-//     .request(api_url)
-//     .put("/restaurant/profile")
-//     .send()
-//     .end(function (res, err) {
-//       expect(200);
-//       done();
-//     });
-// });
+describe("Customer Signup", () => {
+  describe("/Customer/signup", () => {
+    it("Give a array of restaurants", (done) => {
+      let body = {
+        search_string: "pizza",
+      };
+      chai
+        .request(`${routeConstants.BACKEND_URL}`)
+        .get(`/restaurant${routeConstants.GET_RESTAURANT_SEARCH}`)
+        .send(body)
+        .end((err, res) => {
+          expect(200);
+          done();
+        });
+    });
+  });
+});
 
-it("get customer signup") / customer / events / restaurant / createMenuItem;
+describe("Orders for Restaurant", () => {
+  describe("restaurant/orders", () => {
+    it("Give a array of reviews", (done) => {
+      let body = {
+        restaurant_id: 1,
+      };
+      chai
+        .request(`${routeConstants.BACKEND_URL}`)
+        .get(
+          `/reviews${routeConstants.GET_REVIEWS_BY_RESTAURANT}/?restaurant_id=1`
+        )
+        .end((err, res) => {
+          expect(200);
+          done();
+        });
+    });
+  });
+});
